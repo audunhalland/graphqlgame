2e99e584728a32fab0610018553b5af71f574774
Audun Halland <audun.halland@gmail.com>
2019-03-12 21:09:13 +0100

    More stuff

--- src/game/state.ts
+++ src/game/state.ts
@@ -1,15 +1,15 @@
 import * as GeneratePassword from 'generate-password';
-import { start } from 'repl';
 
 const NUMBER_OF_COMPUTER_FILES = 8;
 
+// Clockwise, starting north
 enum Room {
+  LIGHTSWITCH,
   COMPUTER,
-  VERY_DARK,
-  BUTTON,
+  VERYDARK,
   START,
   PASSWORD,
-  SLIGHTLY_NOISY,
+  WIRE,
 }
 
 enum Direction {
@@ -86,34 +86,34 @@ interface ActionResult {
 // Everything listed in clockwise direction, starting out at noon
 export const getRoomNeighbours = (room : Room): RoomNeigbour[] => {
   switch (room) {
-    case Room.COMPUTER: {
+    case Room.LIGHTSWITCH: {
       return [{
         direction: Direction.EAST_SOUTH_EAST,
-        room: Room.VERY_DARK,
+        room: Room.COMPUTER,
       }, {
         direction: Direction.SOUTH_SOUTH_EAST,
-        room: Room.BUTTON,
+        room: Room.VERYDARK,
       }, {
         direction: Direction.SOUTH_SOUTH_WEST,
         room: Room.PASSWORD,
       }, {
         direction: Direction.WEST_SOUTH_WEST,
-        room: Room.SLIGHTLY_NOISY,
+        room: Room.WIRE,
       }];
     }
-    case Room.VERY_DARK: {
+    case Room.COMPUTER: {
       return [{
         direction: Direction.SOUTH,
-        room: Room.BUTTON,
+        room: Room.VERYDARK,
       }, {
         direction: Direction.NORTH_WEST,
-        room: Room.COMPUTER,
+        room: Room.LIGHTSWITCH,
       }];
     }
-    case Room.BUTTON: {
+    case Room.VERYDARK: {
       return [{
         direction: Direction.NORTH,
-        room: Room.VERY_DARK,
+        room: Room.COMPUTER,
       }, {
         direction: Direction.SOUTH_WEST,
         room: Room.START,
@@ -122,13 +122,13 @@ export const getRoomNeighbours = (room : Room): RoomNeigbour[] => {
         room: Room.PASSWORD,
       }, {
         direction: Direction.NORTH_WEST,
-        room: Room.COMPUTER,
+        room: Room.LIGHTSWITCH,
       }];
     }
     case Room.START: {
       return [{
         direction: Direction.NORTH_EAST,
-        room: Room.BUTTON,
+        room: Room.VERYDARK,
       }, {
         direction: Direction.NORTH_WEST,
         room: Room.PASSWORD,
@@ -137,22 +137,22 @@ export const getRoomNeighbours = (room : Room): RoomNeigbour[] => {
     case Room.PASSWORD: {
       return [{
         direction: Direction.NORTH,
-        room: Room.SLIGHTLY_NOISY,
+        room: Room.WIRE,
       }, {
         direction: Direction.NORTH_EAST,
-        room: Room.COMPUTER,
+        room: Room.LIGHTSWITCH,
       }, {
         direction: Direction.EAST,
-        room: Room.BUTTON,
+        room: Room.VERYDARK,
       }, {
         direction: Direction.SOUTH_EAST,
         room: Room.START,
       }];
     }
-    case Room.SLIGHTLY_NOISY: {
+    case Room.WIRE: {
       return [{
         direction: Direction.NORTH_EAST,
-        room: Room.COMPUTER,
+        room: Room.LIGHTSWITCH,
       }, {
         direction: Direction.SOUTH,
         room: Room.PASSWORD,
@@ -163,18 +163,18 @@ export const getRoomNeighbours = (room : Room): RoomNeigbour[] => {
 
 export const getRoomDescription = (state: State, room: Room): string => {
   switch (room) {
+    case Room.LIGHTSWITCH:
+      return state.currentRoom === room
+        ? 'You are in a room with four connecting corridors. On the wall there is a button.'
+        : 'A room with a button on the wall.';
     case Room.COMPUTER:
       return state.currentRoom === room
         ? 'You are in a large and noisy room. In front of you is a big mainframe computer. The noise comes from its large cooling fans. There are four corridors behind you.'
         : 'The computer room.';
-    case Room.VERY_DARK:
+    case Room.VERYDARK:
       return state.currentRoom === room
-        ? 'You are in a very dark room. You cannot see anything but the light from the other ends of the two corridors connecting to the room. There is nothing else here.'
+        ? 'You are in a very dark room. You cannot see anything but the light from the other ends of the four corridors connecting to the room. There is nothing else here.'
         : 'A very dark room';
-    case Room.BUTTON:
-      return state.currentRoom === room
-        ? 'You are in a room with four connecting corridors. On the wall there is a button.'
-        : 'A room with a button on the wall.';
     case Room.START:
       return state.currentRoom === room
         ? 'Oh no. You are trapped in a room. Is it part of a labyrinth? There are two corridors leading out of the room. Behind you is an enormous locked door.'
@@ -191,6 +191,10 @@ export const getRoomDescription = (state: State, room: Room): string => {
           ? 'You are in a dark room, and because there is no lighting, you cannot see anything.'
           : 'A dark room'
         );
+    case Room.WIRE:
+      return state.currentRoom === room
+        ? "You`re in a room again. The room has two entrances. In the ceiling you see an electrical wire that just goes through the room, from the one corridor to the other."
+        : 'The electrical wire room';
     default:
       return 'Nothing of particular interest here.';
   }
@@ -202,8 +206,8 @@ export const getRoomObjects = (state: State, room : Room): GameObject[] => {
       return [{
         type: ObjectType.COMPUTER,
         description: state.hasUnlockedComputer
-          ? 'A large mainframe computer. It is currently locked.'
-          : 'A large mainframe computer. You have successfully unlocked it!',
+          ? 'A large mainframe computer. It is currently prompts for a root password.'
+          : 'A large mainframe computer. You have successfully logged into it as root.',
       }];
     case Room.START:
       return [{
@@ -211,9 +215,10 @@ export const getRoomObjects = (state: State, room : Room): GameObject[] => {
         description: "A big, locked door. It's blocking your exit from the labyrinth. On the door is a big sign with a code on it.",
       }];
     case Room.PASSWORD:
+      if (!state.hasPressedLightSwitch) return [];
       return [{
         type: ObjectType.PASSWORD,
-        description: `A root password. It says "${state.computerPassword}".`,
+        description: `A root password lying on the floor. It says "${state.computerPassword}".`,
       }]
     default:
       return [];
