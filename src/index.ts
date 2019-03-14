992e7db61bd2957f64fe048466cc7d394898ce4a
Audun Halland <audun.halland@gmail.com>
2019-03-14 12:12:37 +0100

    Bind to PORT if defined

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ const server = new ApolloServer({
 const app = express();
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+const port = process.env.PORT || 4000;
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
 );
